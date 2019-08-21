41260c0fecea29dbdf7028877aeb6b0a80f08ca8
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2019-08-20 22:31:28 -0300

    Add User

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,10 +1,10 @@
-module.exports = {
+/*module.exports = {
   env: {
     es6: true,
     node: true,
   },
   extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier']
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -18,6 +18,6 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
-    'no-unused-vars': ['error': { 'argsIgnorePattern': 'next' }]
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
