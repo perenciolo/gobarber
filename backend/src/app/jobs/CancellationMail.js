7470d7989492879aa466a2a72441a104c9322900
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2019-09-03 19:36:14 -0300

    Add exception handling feature

--- backend/src/app/jobs/CancellationMail.js
+++ backend/src/app/jobs/CancellationMail.js
@@ -14,26 +14,22 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    try {
-      await Mail.sendMail({
-        to: `${appointment.provider.name} <${appointment.provider.email}>`,
-        subject: 'Agendamento cancelado',
-        template: 'cancellation',
-        context: {
-          provider: appointment.provider.name,
-          user: appointment.user.name,
-          date: format(
-            parseISO(appointment.date),
-            "'dia' dd 'de' MMMM', às' H'h'mm",
-            {
-              locale: pt,
-            }
-          ),
-        },
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H'h'mm",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://8be6b74c5ff243a2b87836d6732cee78@sentry.io/1550697',
+};
