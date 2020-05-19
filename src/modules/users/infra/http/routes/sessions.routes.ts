6718e33b780ad650c59eb711da6bc1d8364d9a8e
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-05-18 22:22:23 -0300

    Users DDD/SOLID Refactor

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
+  const usersRepository = new UsersRepository();
   const { email, password } = request.body;
 
-  const authUser = new AuthenticateUserService();
+  const authUser = new AuthenticateUserService(usersRepository);
   const { user, token } = await authUser.execute({ email, password });
 
   delete user.password;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -5,13 +5,15 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import uploadConfig from '@config/upload';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
+  const usersRepository = new UsersRepository();
   const { name, email, password } = request.body;
-  const createUser = new CreateUserService();
+  const createUser = new CreateUserService(usersRepository);
   const user = await createUser.execute({ name, email, password });
   delete user.password;
   return response.json(user);
@@ -22,7 +24,8 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const usersRepository = new UsersRepository();
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
       avatarFilename: request.file.filename,
